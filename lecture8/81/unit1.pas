4a978f1f786edd749c4393a0951c33bfe54ec48f
SLOTH <saddolphinboy@gmail.com>
2016-11-21 15:47:28 +0300

    Lecture 8. Task 8.1

---
+++ lecture8/81/unit1.pas
@@ -0,0 +1,44 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, StdCtrls;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    Button1: TButton;
+    Edit1: TEdit;
+    Edit2: TEdit;
+    Label1: TLabel;
+    procedure Button1Click(Sender: TObject);
+  private
+    { private declarations }
+  public
+    { public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.Button1Click(Sender: TObject);
+var
+  m, n:Integer;
+  s:Integer;
+begin
+
+end;
+
+end.
+
